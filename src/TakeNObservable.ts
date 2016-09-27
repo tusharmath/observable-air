b286bc83c529ff60cf5c1f79721b09ff5b648379
Tushar Mathur <tusharmath@gmail.com>
2016-09-27 18:56:38 +0530

    add TakeNObservable

---
+++ src/TakeNObservable.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by tushar.mathur on 27/09/16.
+ */
+
+import {IObservable} from './types/IObservable';
+import {IObserver} from './types/IObserver';
+import {ISubscription} from './types/ISubscription';
+import {Observer} from './Observer';
+
+export class TakeNObservable implements IObservable {
+  private __count: number;
+  private __source: IObservable;
+
+  constructor (count: number, source: IObservable) {
+    this.__count = count
+    this.__source = source
+  }
+
+  subscribe (observer: IObserver): ISubscription {
+    let received = 0
+    const subscription = this.__source.subscribe(Observer.of({
+      next: (val) => {
+        observer.next(val)
+        received++
+        if (received === this.__count) {
+          observer.complete()
+          subscription.unsubscribe()
+        }
+      },
+      error: err => observer.error(err),
+      complete: () => observer.complete()
+    }))
+    return subscription
+  }
+
+}
---
+++ test/test.TakeNObservable.js
@@ -0,0 +1,31 @@
+/**
+ * Created by tushar.mathur on 27/09/16.
+ */
+
+'use strict'
+
+import test from 'ava'
+import {IntervalObservable} from '../.dist/IntervalObservable'
+import U from '../lib/test-util'
+import {TakeNObservable} from '../.dist/TakeNObservable'
+
+test.cb('interval+map', t => {
+  const ob = new TakeNObservable(10, new IntervalObservable(10))
+  const {results} = U.testOB(() => ob)
+  setTimeout(() => {
+    t.deepEqual(results, [
+      {type: 'value', value: 0},
+      {type: 'value', value: 1},
+      {type: 'value', value: 2},
+      {type: 'value', value: 3},
+      {type: 'value', value: 4},
+      {type: 'value', value: 5},
+      {type: 'value', value: 6},
+      {type: 'value', value: 7},
+      {type: 'value', value: 8},
+      {type: 'value', value: 9},
+      {type: 'complete'}
+    ])
+    t.end()
+  }, 1000)
+})
