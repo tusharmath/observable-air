3fbbe728756dc3d9f3993f9973e8de0135f9d262
Tushar Mathur <tusharmath@gmail.com>
2016-09-30 18:13:42 +0530

    make from async

--- src/gen/From.ts
+++ src/gen/From.ts
@@ -5,23 +5,46 @@
 import {IObservable} from '../types/IObservable';
 import {ISubscription} from '../types/ISubscription';
 import {IObserver} from '../types/IObserver';
+import {ITask} from '../types/ITask';
+import {IScheduler} from '../types/IScheduler';
 
 function noop () {
 }
 const unsubscribe = noop
 const subscription = {unsubscribe, closed: true};
-function transmitAll<T> (obr: IObserver<T>, list: Array<T>) {
-  for (var i = 0, l = list.length - 1; i < l; i++) {
-    obr.next(list[i])
+
+
+class FromArrayTask<T> implements ITask {
+  constructor (private obr: IObserver<T>, private list: Array<T>) {
+  }
+
+  run (): void {
+    var list = this.list
+    var l = list.length;
+    var obr = this.obr;
+    for (var i = 0; i < l; ++i) {
+      obr.next(list[i])
+    }
+    obr.complete()
   }
 }
+
+
+class ImmediateScheduler implements IScheduler {
+  run (task: ITask) {
+    setImmediate(() => task.run())
+  }
+}
+
 export class FromObservable<T> implements IObservable<T> {
+  private sh: IScheduler;
+
   constructor (private l: Array<T>) {
+    this.sh = new ImmediateScheduler()
   }
 
   subscribe (obr: IObserver<T>): ISubscription {
-    transmitAll<T>(obr, this.l)
-    obr.complete()
+    this.sh.run(new FromArrayTask(obr, this.l))
     return subscription
   }
 }
---
+++ src/types/IScheduler.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by tushar.mathur on 30/09/16.
+ */
