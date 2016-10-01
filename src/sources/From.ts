d938b96cd1f8e3ac0efcf37d75aa11abe3c82571
Tushar Mathur <tusharmath@gmail.com>
2016-10-01 22:05:18 +0530

    use inlining cache stubs

--- src/sources/From.ts
+++ src/sources/From.ts
@@ -5,36 +5,21 @@
 import {IObservable} from '../types/IObservable';
 import {ISubscription} from '../types/ISubscription';
 import {IObserver} from '../types/IObserver';
-import {ITask} from '../types/ITask';
-import {IScheduler} from '../types/IScheduler';
-import {NowScheduler} from '../schedulers/NowScheduler';
 
 const unsubscribe = function () {
 }
 const subscription = {unsubscribe, closed: true};
 
-
-class FromArrayTask<T> implements ITask {
-  constructor (private observer: IObserver<T>, private array: Array<T>) {
-  }
-
-  run (): void {
-    var len = this.array.length;
-    for (var i = 0; i < len; ++i) {
-      this.observer.next(this.array[i])
-    }
-    this.observer.complete()
-  }
-}
-
-
 export class FromObservable<T> implements IObservable<T> {
-  constructor (private array: Array<T>,
-               private scheduler: IScheduler<void> = new NowScheduler()) {
+  constructor (private array: Array<T>) {
   }
 
-  subscribe (obr: IObserver<T>): ISubscription {
-    this.scheduler.run(new FromArrayTask(obr, this.array))
+  subscribe (observer: IObserver<T>): ISubscription {
+    const end = () => observer.complete()
+    for (var i = 0; i < this.array.length; ++i) {
+      observer.next(this.array[i])
+    }
+    end()
     return subscription
   }
 }
