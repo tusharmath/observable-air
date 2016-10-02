9ab2121ba5bbf9ae30a31a49d12b6527bc42b20c
Tushar Mathur <tusharmath@gmail.com>
2016-10-02 15:57:02 +0530

    add tap operator

---
+++ src/operators/Tap.ts
@@ -0,0 +1,45 @@
+/**
+ * Created by tushar.mathur on 02/10/16.
+ */
+
+import {IObservable} from '../types/IObservable';
+import {IObserver} from '../types/IObserver';
+import {ISubscription} from '../types/ISubscription';
+
+interface ITapper<T> {
+  (f: T): void
+}
+
+export class TapObserver<T> implements IObserver<T> {
+  constructor (private tapper: ITapper<T>, private observer: IObserver<T>) {
+
+  }
+
+  next (val: T): void {
+    this.tapper(val)
+    this.observer.next(val)
+  }
+
+  error (err: Error): void {
+    this.observer.error(err)
+  }
+
+  complete (): void {
+    this.observer.complete()
+  }
+
+}
+
+export class TapObservable<T> implements IObservable<T> {
+  constructor (private tapper: ITapper<T>, private source: IObservable<T>) {
+
+  }
+
+  subscribe (observer: IObserver<T>): ISubscription {
+    return this.source.subscribe(new TapObserver(this.tapper, observer));
+  }
+}
+
+export function tap<T> (tapper: ITapper<T>, source: IObservable<T>): TapObservable<T> {
+  return new TapObservable(tapper, source)
+}
