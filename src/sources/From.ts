76043af16617dca2f0a00e746d2773728fca7b79
Tushar Mathur <tusharmath@gmail.com>
2016-10-03 18:25:55 +0530

    remove from2

--- src/sources/From.ts
+++ src/sources/From.ts
@@ -23,23 +23,6 @@ export class FromObservable<T> implements IObservable<T> {
   }
 }
 
-export class From2Observable<T> implements IObservable<T> {
-  constructor (private array: Array<T>) {
-  }
-
-  subscribe (observer: IObserver<T>): ISubscription {
-    for (var i = 0; i < this.array.length; ++i) {
-      observer.next(this.array[i])
-    }
-    observer.complete()
-    return subscription
-  }
-}
-
 export function from<T> (list: Array<T>): IObservable<T> {
   return new FromObservable(list)
-}
-
-export function from2<T> (list: Array<T>): IObservable<T> {
-  return new From2Observable(list)
 }
