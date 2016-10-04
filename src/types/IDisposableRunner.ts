260545e1905b09f2fca4fe14b57e93bf75e8e7fc
Tushar Mathur <tusharmath@gmail.com>
2016-10-04 14:17:56 +0530

    white space changes

--- src/types/IDisposableRunner.ts
+++ src/types/IDisposableRunner.ts
@@ -1,7 +1,8 @@
-import {IDisposable} from './IDisposable';
 /**
  * Created by tushar.mathur on 04/10/16.
  */
+
+import {IDisposable} from './IDisposable';
 
 export interface IDisposableRunner extends IDisposable {
   run (): IDisposableRunner
