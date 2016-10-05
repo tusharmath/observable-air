1f787db52d2e511300cd1040c476504ad3da2ce0
Tushar Mathur <tusharmath@gmail.com>
2016-10-05 18:02:13 +0530

    create SubscriptionObserver

--- src/Observable.ts
+++ src/Observable.ts
@@ -5,41 +5,30 @@
 import {IObservable} from './core-types/IObservable';
 import {ISubscription} from './core-types/ISubscription';
 import {ISubscriberFunction} from './core-types/ISubscriberFunction';
-import {IObserver} from './core-types/IObserver';
-import {Observer} from './Observer';
-
-
-class SubscriptionWrapper<T> implements ISubscription {
-  private __closed: boolean
-
-  constructor (private func: () => void) {
-    this.__closed = false
-  }
-
-  unsubscribe (): void {
-    this.func()
-    this.__closed = true
-  }
-
-  get closed () {
-    return this.__closed
-  }
-}
+import {
+  IObserver,
+  INextFunction,
+  IErrorFunction,
+  ICompleteFunction
+} from './core-types/IObserver';
+import {SubscriptionObserver} from './SubscriptionObserver';
+import {Subscription} from './Subscription';
 
 
 export class Observable<T> implements IObservable<T> {
+  constructor (private func: ISubscriberFunction<T>) {
+  }
+
   static of<U> (subscriberFunction: ISubscriberFunction<U>) {
     return new Observable(subscriberFunction)
   }
 
-  constructor (private func: ISubscriberFunction<T>) {
-  }
+  subscribe (onNext: IObserver<T> | INextFunction<T>,
+             onError?: IErrorFunction,
+             onComplete?: ICompleteFunction): ISubscription {
 
-  subscribe (observer: IObserver<T>): ISubscription {
-    const subscription = this.func(new Observer(observer))
-    if (typeof subscription === 'function') {
-      return new SubscriptionWrapper(subscription as (() => void))
-    }
-    return subscription as ISubscription
+    return Subscription.from(this.func(
+      SubscriptionObserver.from(onNext, onError, onComplete)
+    ))
   }
 }
