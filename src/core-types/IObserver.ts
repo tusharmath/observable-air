0e2f6ccd61a8034044b9b3bdec13246a18bcef09
Tushar Mathur <tusharmath@gmail.com>
2016-10-05 16:34:39 +0530

    add function types in IObserver

--- src/core-types/IObserver.ts
+++ src/core-types/IObserver.ts
@@ -2,8 +2,21 @@
  * Created by tushar.mathur on 27/09/16.
  */
 
+
+export interface INextFunction<T> {
+  (val: T): void
+}
+
+export interface IErrorFunction {
+  (err: Error): void
+}
+
+export interface ICompleteFunction {
+  (): void
+}
+
 export interface IObserver<T> {
-  next(val: T): void
-  error(err: Error): void
-  complete(): void
+  next: INextFunction<T>
+  error: IErrorFunction
+  complete: ICompleteFunction
 }
