fccce5d1ea6e81eda8e9f925abbfda44113d5ae4
Tushar Mathur <tusharmath@gmail.com>
2016-10-08 20:37:49 +0530

    fix(SubscriptionObserver): handle more observer types
    observers could be of function type

--- src/SubscriptionObserver.ts
+++ src/SubscriptionObserver.ts
@@ -5,42 +5,45 @@
 import {ISubscriptionObserver} from './types/core/ISubscriptionObserver';
 import {IObserver} from './types/core/IObserver';
 
-export class SubscriptionObserverStub <T> implements ISubscriptionObserver<T> {
-  closed: boolean;
-
-  next (val: T): void {
-  }
-
-  error (err: Error): void {
-  }
-
-  complete (): void {
-  }
+export function noop () {
 }
+export function defaultOnError (err: Error) {
+  throw err
+}
+
 
 export class SubscriptionObserver<T> implements ISubscriptionObserver<T> {
   closed: boolean;
 
-  constructor (private sink: IObserver<T>) {
+  constructor (private onNext: (val: T) => void,
+               private onError: (err: Error) => void,
+               private onComplete: () => void) {
     this.closed = false
   }
 
   next (val: T): void {
-    this.sink.next(val)
+    this.onNext(val)
   }
 
   error (err: Error): void {
-    this.sink.error(err)
+    this.onError(err)
   }
 
   complete (): void {
-    this.sink.complete()
+    this.onComplete()
     this.closed = true
   }
 
-  static from<T> (observer: IObserver<T>) {
-    if (!observer.next || !observer.complete || !observer.error)
-      return new SubscriptionObserverStub()
-    return new SubscriptionObserver(observer)
+  static from<T> (observer: IObserver<T> | ((t: T) => void)): ISubscriptionObserver<T> {
+    const type = typeof observer
+    if (type !== 'object' && type !== 'function') throw new TypeError()
+    if (observer instanceof SubscriptionObserver) return observer
+    if (typeof observer === 'function')
+      return new SubscriptionObserver(observer, defaultOnError, noop)
+    return new SubscriptionObserver(
+      observer.next ? (t: T) => observer.next(t) : noop,
+      observer.error ? (t: Error) => observer.error(t) : defaultOnError,
+      observer.complete ? () => observer.complete() : noop
+    )
   }
 }
