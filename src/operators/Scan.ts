d3677f6d47077f666666401b2d93c3667212179d
Tushar Mathur <tusharmath@gmail.com>
2016-10-09 13:45:51 +0530

    feat(scan): add scan operator

---
+++ src/operators/Scan.ts
@@ -0,0 +1,46 @@
+/**
+ * Created by tushar.mathur on 09/10/16.
+ */
+
+
+import {IObservable} from '../types/core/IObservable';
+import {IObserver} from '../types/core/IObserver';
+import {IScheduler} from '../types/IScheduler';
+import {ISubscription} from '../types/core/ISubscription';
+import {IReducer} from '../types/IReducer';
+
+export class ScanObserver<T> implements IObserver<T> {
+
+  constructor (private reducer: IReducer<T>,
+               private value: T,
+               private sink: IObserver<T>) {
+  }
+
+  next (val: T): void {
+    this.value = this.reducer(this.value, val);
+    this.sink.next(this.value)
+  }
+
+  error (err: Error): void {
+    this.sink.error(err)
+  }
+
+  complete (): void {
+    this.sink.complete()
+  }
+
+}
+
+export class ScanObservable<T> implements IObservable<T> {
+  constructor (private reducer: IReducer<T>, private value: T, private source: IObservable<T>) {
+
+  }
+
+  subscribe (observer: IObserver<T>, scheduler: IScheduler): ISubscription {
+    return this.source.subscribe(new ScanObserver(this.reducer, this.value, observer), scheduler)
+  }
+}
+
+export function scan<T> (reducer: IReducer<T>, value: T, source: IObservable<T>) {
+  return new ScanObservable(reducer, value, source)
+}
