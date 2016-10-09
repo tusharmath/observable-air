6154818b288edc39dff8c685f5dcc474a1eb6070
Tushar Mathur <tusharmath@gmail.com>
2016-10-09 12:23:55 +0530

    refactor(Interval): remove interval observer

--- src/sources/Interval.ts
+++ src/sources/Interval.ts
@@ -7,29 +7,9 @@ import {ISubscription} from '../types/core/ISubscription';
 import {IObserver} from '../types/core/IObserver';
 import {IScheduler} from '../types/IScheduler';
 import {DefaultScheduler} from '../scheduling/DefaultScheduler';
-import {ILazySubscription} from '../types/ILazySubscription';
 import {SafeExecutor} from '../lib/SafeExecutor';
 import {PassOnError} from '../lib/PassOnError';
 
-
-export class IntervalObserver implements ILazySubscription {
-  closed: boolean;
-  private count: number;
-
-  constructor (private sink: IObserver<number>) {
-    this.count = 0
-    this.closed = false
-  }
-
-  run (): ILazySubscription {
-    this.sink.next(this.count++)
-    return this;
-  }
-
-  unsubscribe (): void {
-    this.closed = true
-  }
-}
 
 export class IntervalObservable<Number> implements IObservable<number> {
   constructor (private interval: number) {
