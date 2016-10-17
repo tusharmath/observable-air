ad2e4d6d86c74693bdb82bff274eb8e8a9cec643
Tushar Mathur <tusharmath@gmail.com>
2016-10-17 17:38:47 +0530

    fix(curry): call on arg.length >= func.len

--- src/lib/Curry.ts
+++ src/lib/Curry.ts
@@ -5,10 +5,11 @@
 
 import {ICurryFunction} from '../types/ICurryFunction'
 
-export function Curry <T, R> (f: ICurryFunction<T>): ICurryFunction<T> {
+export function Curry<T, R> (f: ICurryFunction<T>): ICurryFunction<T> {
+  if (f.length === 1) return f
   return function curried (...t: any[]): T | ICurryFunction<T> {
     if (t.length === 0) return curried
-    if (t.length === f.length) return f(...t)
+    if (t.length >= f.length) return f(...t)
     return curried.bind(this, ...t)
   }
 }
