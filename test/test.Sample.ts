17ddbc2a9939558f435ab01e8606da50569ed9e0
Tushar Mathur <tusharmath@gmail.com>
2016-10-22 16:58:44 +0530

    fix(Sample): fix mapper call
    mapper needs to be applied to the arguments

--- test/test.Sample.ts
+++ test/test.Sample.ts
@@ -8,7 +8,7 @@ import {ReactiveEvents} from '../src/testing/ReactiveEvents'
 import {sample} from '../src/operators/Sample'
 
 function toArray(...t: Array<any>) {
-  return t.join('')
+  return t.join(',')
 }
 test(t => {
   const sh = new TestScheduler()
@@ -43,3 +43,38 @@ test(t => {
     ReactiveEvents.complete(251)
   ])
 })
+
+test(t => {
+  const sh = new TestScheduler()
+  const a$ = sh.Hot([
+    ReactiveEvents.next(210, 0),
+    ReactiveEvents.next(230, 1),
+    ReactiveEvents.next(250, 2),
+    ReactiveEvents.complete(250)
+  ])
+  const b$ = sh.Hot([
+    ReactiveEvents.next(210, 0),
+    ReactiveEvents.next(220, 1000),
+    ReactiveEvents.next(230, 2000),
+    ReactiveEvents.next(240, 3000),
+    ReactiveEvents.complete(240)
+  ])
+  const S$ = sh.Hot([
+    ReactiveEvents.next(211, '#'),
+    ReactiveEvents.next(221, '#'),
+    ReactiveEvents.next(231, '#'),
+    ReactiveEvents.next(241, '#'),
+    ReactiveEvents.next(251, '#'),
+    ReactiveEvents.complete(251)
+  ])
+  const {results} = sh.start(() => sample<number>((a, b) => a + b, S$, [a$, b$]))
+  t.deepEqual(results, [
+    ReactiveEvents.next(211, 0 + 0),
+    ReactiveEvents.next(221, 0 + 1000),
+    ReactiveEvents.next(231, 1 + 2000),
+    ReactiveEvents.next(241, 1 + 3000),
+    ReactiveEvents.next(251, 2 + 3000),
+    ReactiveEvents.complete(251)
+  ])
+})
+
