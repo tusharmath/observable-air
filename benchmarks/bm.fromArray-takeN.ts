e895fec89c6092d25de9565797e244f395eb7b64
Tushar Mathur <tusharmath@gmail.com>
2016-11-06 14:06:25 +0530

    refactor(benchmarks): use IDeferred type

--- benchmarks/bm.fromArray-takeN.ts
+++ benchmarks/bm.fromArray-takeN.ts
@@ -4,14 +4,15 @@
 
 import {Suite} from 'benchmark'
 import {fromArray} from '../src/sources/FromArray'
-import {IDefered, run, array} from './lib'
+import {run, array, IDeferred} from './lib'
 import {slice} from '../src/operators/Slice'
 
 const a = array(1e6)
 export function fromArray_takeN (suite: Suite) {
-  return suite.add('file -> takeN(0, n/10)',
-    (d: IDefered) =>
-      run(slice(0, 1e6 / 10, fromArray(a)), d),
-    {defer: true}
-  )
+  return suite
+    .add(
+      'file -> takeN(0, n/10)',
+      (d: IDeferred) => run(slice(0, 1e6 / 10, fromArray(a)), d),
+      {defer: true}
+    )
 }
