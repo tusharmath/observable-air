adae96cf254d571c084df0a6572b4233ade3be93
Tushar Mathur <tusharmath@gmail.com>
2016-11-06 13:55:40 +0530

    refactor(benchmarks): display date of the run

--- benchmarks/run.ts
+++ benchmarks/run.ts
@@ -7,16 +7,19 @@ import {fromArray_map_reduce} from './bm.fromArray-map-reduce'
 import {fromArray_scan_reduce} from './bm.fromArray-scan-reduce'
 import {fromArray_takeN} from './bm.fromArray-takeN'
 import {onCycle} from './lib'
+import {tryCatch} from './bm.tryCatch'
 
 // Run All Benchmarks
 
 console.log('Node:', process.version)
 console.log('V8:  ', process.versions.v8)
+console.log('Date:', new Date())
 console.log('----------------')
 
 const suite = new Suite()
 fromArray_map_reduce(suite)
 fromArray_scan_reduce(suite)
 fromArray_takeN(suite)
-
+tryCatch(suite)
 suite.on('cycle', onCycle).run()
+
