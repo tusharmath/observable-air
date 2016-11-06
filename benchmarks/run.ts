b98564fbe7408c876e5ceaaad2df1dc192d3bfa8
Tushar Mathur <tusharmath@gmail.com>
2016-11-06 15:31:16 +0530

    refactor(benchmarks): add save script and display date

--- benchmarks/run.ts
+++ benchmarks/run.ts
@@ -11,15 +11,16 @@ import {tryCatch} from './bm.tryCatch'
 
 // Run All Benchmarks
 
+console.log('')
+console.log('Date:', (new Date()).toDateString())
 console.log('Node:', process.version)
 console.log('V8:  ', process.versions.v8)
-console.log('Date:', new Date())
-console.log('----------------')
+console.log('------------------------------')
 
 const suite = new Suite()
 fromArray_map_reduce(suite)
-fromArray_scan_reduce(suite)
 fromArray_takeN(suite)
+fromArray_scan_reduce(suite)
 tryCatch(suite)
 suite.on('cycle', onCycle).run()
 
