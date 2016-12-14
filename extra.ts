7291134448328a62b364a6feaff540bb183076d3
Tushar Mathur <tusharmath@gmail.com>
2016-12-14 14:42:01 +0530

    fix(main): remove optional operators/sources from main and add them to extra

---
+++ extra.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by tushar on 10/12/16.
+ */
+
+export {combine} from './src/operators/Combine'
+export {fromArray} from './src/sources/FromArray'
+export {rafThrottle} from './src/operators/RafThrottle'
+export {tap} from './src/operators/Tap'
+export {TestScheduler} from './src/testing/TestScheduler'
