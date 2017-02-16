6735a4cb9b09b0a82f4929b6ea73881771e435e4
Tushar Mathur <tusharmath@gmail.com>
2017-02-16 10:24:33 +0530

    refactor(operator): update imports

--- src/operators/Operator.ts
+++ src/operators/Operator.ts
@@ -1,8 +1,8 @@
 /**
  * Created by tushar on 16/02/17.
  */
-import {Subscription} from '../types/core/Subscription'
-import {Observer} from '../types/core/Observer'
+import {Subscription} from '../lib/Subscription'
+import {Observer} from '../lib/Observer'
 
 export interface Operator<T> extends Subscription, Observer<T> {
 }
