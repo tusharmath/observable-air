14cc7bf492c31ee0af0c51411ccd0bd8f7955e79
Tushar Mathur <tusharmath@gmail.com>
2017-02-26 20:33:06 +0530

    chore(gh-pages): skip ci build on commit

--- scripts/gh-pages.ts
+++ scripts/gh-pages.ts
@@ -6,6 +6,8 @@ const {version} = require('../package.json')
 const ghPages = require('gh-pages')
 const path = require('path')
 
-ghPages.publish(path.join(__dirname, {message: `docs(gh-pages): publishing version ${version}`}, '../doc'), function (err: Error) {
+const message = `docs(gh-pages): publishing version ${version}
+[skip ci]`
+ghPages.publish(path.join(__dirname, {message: message}, '../doc'), function (err: Error) {
   throw err
 })
