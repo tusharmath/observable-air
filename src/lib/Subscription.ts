c0e75a48794805d0c3ca830d8f20aa52dc7a3745
Tushar Mathur <tusharmath@gmail.com>
2017-02-26 19:27:10 +0530

    refactor(lib): combine Basesubscription into subscription

--- src/lib/Subscription.ts
+++ src/lib/Subscription.ts
@@ -2,7 +2,34 @@
  * Created by tushar.mathur on 27/09/16.
  */
 
+import {Subscription} from './Subscription'
+
 export interface Subscription {
   unsubscribe(): void
   readonly closed: boolean
 }
+
+export function isSubscription (subscription: Subscription) {
+  return subscription instanceof BaseSubscription || (subscription && typeof subscription.unsubscribe === 'function')
+}
+
+export class BaseSubscription implements Subscription {
+  constructor (private f: (() => void), public closed = false) {
+  }
+
+  unsubscribe (): void {
+    this.f()
+    this.closed = true
+  }
+
+  static from (subscription: Subscription | {(): void} | void): Subscription {
+    if (isSubscription(subscription as Subscription))
+      return subscription as Subscription
+
+    if (typeof subscription === 'function')
+      return new BaseSubscription(subscription as {(): void})
+
+    return new BaseSubscription(() => undefined)
+  }
+}
+
