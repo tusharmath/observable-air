6d890d31c6a308e4f67b7b5b2a6d9cf033baea67
Tushar Mathur <tusharmath@gmail.com>
2017-03-19 14:27:47 +0530

    build(tsc): create tsc files in the same dir as the source files

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   exports: 'named',
-  entry: './.dist/src/main.js',
+  entry: './src/main.js',
   dest: './.dist/main-es.js',
   plugins: [
     commonjs({})
