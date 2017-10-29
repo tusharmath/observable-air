1280e0c0df0e5f44fc77c458b1ced0769b255e52
Tushar Mathur <tusharmath@gmail.com>
2017-10-29 14:19:06 +0530

    refactor(foreach): move it to sinks directory

--- extra.ts
+++ extra.ts
@@ -2,5 +2,7 @@
  * Created by tushar on 10/12/16.
  */
 
+export * from './src/main'
 export {combine} from './src/operators/Combine'
 export {debounce} from './src/operators/Debounce'
+export {toNodeStream, fromNodeStream} from './src/sinks/NodeStream'
