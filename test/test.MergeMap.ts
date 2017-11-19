0721de71627a255ef144c1447c266e1725b7d9c0
Tushar Mathur <tusharmath@gmail.com>
2017-11-19 13:04:56 +0530

    refactor(mergeMap): use marbles for testing

--- test/test.MergeMap.ts
+++ test/test.MergeMap.ts
@@ -3,7 +3,6 @@
  */
 import * as assert from 'assert'
 import {mergeMap} from '../src/operators/MergeMap'
-import {just} from '../src/sources/Create'
 import {EVENT} from '../src/testing/Events'
 import {createTestScheduler} from '../src/testing/TestScheduler'
 
@@ -12,34 +11,26 @@ describe('mergeMap()', () => {
   context('when concurrency is Infinity', () => {
     it('should work like flatMap()', () => {
       const sh = createTestScheduler()
-      const sa$$ = sh.Cold([
-        next(10, 'A0'),
-        next(20, 'A1'),
-        next(30, 'A2'),
-        complete(40)
-      ])
+      const conc = '    -3|                      '
+      const a = '       --A---A|                 '
+      const b = '       ---B---B---B---B|        '
+      const c = '       ----C---C---C---C---C---|'
+      const expected = '--ABC-ABC--BC--BC---C---|'
 
-      const sb$$ = sh.Cold([
-        next(10, 'B0'),
-        next(20, 'B1'),
-        next(30, 'B2'),
-        complete(40)
-      ])
-
-      const s$$ = sh.Cold(next(10, sa$$), next(20, sb$$), complete(100))
-      const {results} = sh.start(() =>
-        mergeMap(just(Infinity), (i: any) => i, s$$)
-      )
-
-      assert.deepEqual(results, [
-        next(220, 'A0'),
-        next(230, 'A1'),
-        next(230, 'B0'),
-        next(240, 'A2'),
-        next(240, 'B1'),
-        next(250, 'B2'),
-        complete(300)
-      ])
+      const observer = sh.start(() => {
+        const conc$ = sh.Hot(conc)
+        const a$ = sh.Hot(a)
+        const b$ = sh.Hot(b)
+        const c$ = sh.Hot(c)
+        const source$$ = sh.Hot(
+          next(200, a$),
+          next(215, b$),
+          next(220, c$),
+          complete(300)
+        )
+        return mergeMap(conc$, (i: any) => i, source$$)
+      })
+      assert.deepEqual(observer.toString(), expected)
     })
   })
   context('when concurrency is 1', () => {
