c6309df2e080a6320d49c5bbc5c7de4f19c700a1
Tushar Mathur <tusharmath@gmail.com>
2018-03-20 13:41:04 +0530

    style(prettier): whitespace changes

--- src/lib/SafeObserver.ts
+++ src/lib/SafeObserver.ts
@@ -1,9 +1,9 @@
 /**
  * Created by tushar on 29/01/17.
  */
-import { CompleteMixin, ErrorMixin, Virgin } from './Mixins'
-import { IObserver } from './Observer'
-import { ISafeFunction, tryCatch } from './Utils'
+import {CompleteMixin, ErrorMixin, Virgin} from './Mixins'
+import {IObserver} from './Observer'
+import {ISafeFunction, tryCatch} from './Utils'
 
 export interface ISafeObserver<T> extends IObserver<T> {
   erred: boolean
