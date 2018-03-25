b7ceeef54e6a4123c37a78d2c4fb73115d9cf5ff
Tushar Mathur <tusharmath@gmail.com>
2018-03-25 14:44:31 +0530

    refactor(testScheduler): rename variables in test file

--- test/test.TestScheduler.ts
+++ test/test.TestScheduler.ts
@@ -1,23 +1,23 @@
 /**
  * Created by tushar on 28/01/17.
  */
-import * as t from 'assert'
+import * as assert from 'assert'
 import {EVENT} from '../src/internal/Events'
 import {createTestScheduler} from '../src/schedulers/TestScheduler'
 
 describe('new TestScheduler()', () => {
   it('should just ... ', () => {
     const sh = createTestScheduler()
-    t.strictEqual(sh.now(), 0)
+    assert.strictEqual(sh.now(), 0)
 
     sh.advanceBy(10)
-    t.strictEqual(sh.now(), 10)
+    assert.strictEqual(sh.now(), 10)
 
     sh.advanceBy(2)
-    t.strictEqual(sh.now(), 12)
+    assert.strictEqual(sh.now(), 12)
 
     sh.advanceTo(20)
-    t.strictEqual(sh.now(), 20)
+    assert.strictEqual(sh.now(), 20)
   })
 
   it('should just ... ', () => {
@@ -31,7 +31,7 @@ describe('new TestScheduler()', () => {
       ])
     )
 
-    t.deepEqual(results, [
+    assert.deepEqual(results, [
       EVENT.next(210, '0'),
       EVENT.next(220, '1'),
       EVENT.next(230, '2'),
